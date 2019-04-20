ce69206c3af904d2e5cc16e6c2ce119e2b519c62
CSatriale <36242838+CSatriale@users.noreply.github.com>
2019-04-20 17:29:44 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,7 +1,6 @@
 '''
 PS#2
 Q3 Inception Module for CIFAR-10 dataset
-
 '''
 import keras
 from keras.models import Model
@@ -11,7 +10,7 @@ from keras.layers import Input
 from keras.utils import np_utils
 from keras.datasets import cifar10
 
-epochs = 50
+epochs = 100
 
 # Get the data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -30,25 +29,20 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
 
-volume_2 = Conv2D(32, (1,1), padding='same', activation='relu')(input_img)
-volume_2 = Conv2D(64, (3,3), padding='same', activation='relu')(volume_2)
+volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_2)
 
-volume_3 = Conv2D(32, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(64, (5,5), padding='same', activation='relu')(volume_3)
+volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
 
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
 volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 
-
-
 # Concatenate all volumes of the Inception module
-inception_module = keras.layers.Dropout(rate, noise_shape=None, seed=None)
-
-# keras.layers.concatenate([volume_1, volume_2, volume_3,
-                                             # volume_4], axis = 3)
-
+inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
+                                             volume_4], axis = 3)
 output = Flatten()(inception_module)
 out    = Dense(10, activation='softmax')(output)
 
@@ -57,10 +51,8 @@ model = Model(inputs = input_img, outputs = out)
 print(model.summary())
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=256)
+hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=512)
 
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
-
