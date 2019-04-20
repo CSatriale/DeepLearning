f5b7e23942169c1423074f9f44d60a527cbf6c49
CSatriale <36242838+CSatriale@users.noreply.github.com>
2019-04-20 17:09:17 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -30,13 +30,13 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+volume_1 = Conv2D(32, (1,1), padding='same', activation='relu')(input_img)
 
 volume_2 = Conv2D(32, (1,1), padding='same', activation='relu')(input_img)
 volume_2 = Conv2D(64, (3,3), padding='same', activation='relu')(volume_2)
 
-volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
+volume_3 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
+volume_3 = Conv2D(128, (5,5), padding='same', activation='relu')(volume_3)
 
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
 volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
@@ -45,7 +45,7 @@ volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
                                              volume_4], axis = 3)
 output = Flatten()(inception_module)
-out    = Dense(20, activation='softmax')(output)
+out    = Dense(10, activation='softmax')(output)
 
 
 model = Model(inputs = input_img, outputs = out)
