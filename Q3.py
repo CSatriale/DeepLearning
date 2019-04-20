56a258ff21bd6f085e821fa26b25d6b6d642797b
CSatriale <36242838+CSatriale@users.noreply.github.com>
2019-04-20 17:26:13 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -41,11 +41,14 @@ volume_3 = Conv2D(64, (5,5), padding='same', activation='relu')(volume_3)
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
 volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 
-keras.layers.Dropout(rate, noise_shape=None, seed=None)
+
 
 # Concatenate all volumes of the Inception module
-inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
-                                             volume_4], axis = 3)
+inception_module = keras.layers.Dropout(rate, noise_shape=None, seed=None)
+
+# keras.layers.concatenate([volume_1, volume_2, volume_3,
+                                             # volume_4], axis = 3)
+
 output = Flatten()(inception_module)
 out    = Dense(10, activation='softmax')(output)
 
