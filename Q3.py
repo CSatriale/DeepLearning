b83bab1bf685a6d828cdc570f9ca856de82bca01
CSatriale <36242838+CSatriale@users.noreply.github.com>
2019-04-21 20:36:34 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -9,6 +9,13 @@ from keras.layers import Flatten, Dense
 from keras.layers import Input
 from keras.utils import np_utils
 from keras.datasets import cifar10
+from keras.optimizers import SGD
+
+lrate = 0.01
+
+decay = lrate/epochs
+
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 
 epochs = 100
 
@@ -52,7 +59,7 @@ out    = Dense(10, activation='softmax')(output)
 model = Model(inputs = input_img, outputs = out)
 print(model.summary())
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=512)
 
 
