418bbb9ada21ed29b2a34f05c3984059bbd29bfb
CSatriale <36242838+CSatriale@users.noreply.github.com>
2019-04-21 21:13:04 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -54,7 +54,7 @@ model = Model(inputs = input_img, outputs = out)
 print(model.summary())
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=512)
+hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
 
 
 scores = model.evaluate(X_test, y_test, verbose=0)
